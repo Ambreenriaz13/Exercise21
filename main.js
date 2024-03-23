1e9031652c567995d20c3e2a5968546ffa6b2d71
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-03-23 23:16:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var itcource = {
+    courseName: "TypeScript and JavaScript",
+    location: "Governor",
+    onSiteStudent: 50000,
+};
+console.log(itcource);
---
+++ main.ts
@@ -0,0 +1,14 @@
+interface itcource{
+    courseName: string;
+    location: string;
+    onSiteStudent: number;
+}
+
+let itcource = {
+    courseName : "TypeScript and JavaScript",
+    location : "Governor",
+    onSiteStudent : 50000,
+
+}
+
+console.log(itcource);
